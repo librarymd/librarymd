a803d1427fc96b973f909cb808bc648b057b9ec0
XXN <dan15i@yahoo.com>
2019-01-25 14:00:34 +0200

    Update mytorrents.php_ru.php

--- webapp/lang/mytorrents.php_ru.php
+++ webapp/lang/mytorrents.php_ru.php
@@ -1,7 +1,7 @@
 <?php
 $lang['browse_added_hours_ago'] = 'чac.';
 $lang['browse_added_mins_ago'] = 'мин.';
-$lang['Nu-s torrente'] = 'Нет торрентов';
+$lang['Nu aveţi torrente'] = 'Нет торрентов';
 $lang['Nu aţi încărcat nici un torrent încă, aşa că pagina este goală'] = 'Вы ещё не загрузили ни одного торрента, поэтому страница пуста.';
 $lang['Editează'] = 'Редакт.';
 $lang['Vizibil'] = 'Видимый';
